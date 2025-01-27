69b50826fbcd60db39e3b3e30f9021b549478a93
CodeDevOption <codedevoption@gmail.com>
2025-01-27 12:01:10 +0530

    Sanity installed

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,23 +2,59 @@ import { formatDate } from "@/lib/utils";
 import React from "react";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
-const StartupCard = ({ post }: formatDate) => {
-  console.log(post);
+import Image from "next/image";
+import { Button } from "./ui/button";
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author: { _id: author_id, name },
+    _id,
+    description,
+    image,
+    category,
+    title,
+  } = post;
+
   return (
     <li className="startup-card group">
       <div className="flex-between">
-        <p className="startup_card_date">{formatDate(post._createdAt)}</p>
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
         <div className="flex gap-1.5">
           <EyeIcon className="size-6  text-primary" />
-          <span className="text-16-medium">{post.views}</span>
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${post.author?._id}`}>
-            <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+          <Link href={`/user/${author_id}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startup/${post.star}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
+        <Link href={`/user/${author_id}`}>
+          <Image
+            src="https://placehold.co/48x48"
+            alt="placeholder"
+            className="rounded-full"
+            width={48}
+            height={48}
+          />
+        </Link>
+      </div>
+      <Link href={`/startups/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        <img src={image} alt="placeholder" className="startup-card_img" />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`startup/${_id}`}>Details</Link>
+        </Button>
       </div>
     </li>
   );
